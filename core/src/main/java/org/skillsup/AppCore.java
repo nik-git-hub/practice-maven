49a07c6454659159a7aa36186eac51c8998ac5a8
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2020-06-20 22:41:27 +0300

    initial commit

---
+++ core/src/main/java/org/skillsup/AppCore.java
@@ -0,0 +1,10 @@
+package org.skillsup;
+
+/**
+ * Core module.
+ */
+public class AppCore {
+     public static void main(String[] args) {
+         System.out.println("Core module");
+        }
+}
---
+++ dao/src/main/java/org/skillsup/AppDao.java
@@ -0,0 +1,10 @@
+package org.skillsup;
+
+/**
+ * Dao module.
+ */
+public class AppDao {
+    public static void main(String[] args) {
+        System.out.println("Dao module");
+    }
+}
